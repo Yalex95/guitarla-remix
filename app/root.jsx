f0ec330330e6a22fd47e684e55e0447a57661e01
yerisaguilar <yerisaguilarg@gmail.com>
2023-10-12 13:07:41 -0700

    add internal stylesheets

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,8 @@
-import {Meta} from '@remix-run/react'
-
+import {
+    Meta,
+    Links
+} from '@remix-run/react'
+import styles from './styles/index.css'
 export function meta() {
     return [
       { charset: "utf-8" },
@@ -11,7 +14,8 @@ export function meta() {
 export function links(){
     return[
         {
-            
+            rel:'stylesheet',
+            href:styles
         }
     ]
 }
@@ -28,6 +32,7 @@ function Document({children}){
         <html lang="es">
             <head>
                 <Meta/>
+                <Links/>
             </head>
             <body>
                 {children}

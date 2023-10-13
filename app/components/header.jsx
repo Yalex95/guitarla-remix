21047219c0ca0a9c715facbe96f7d3615dec0c26
yerisaguilar <yerisaguilarg@gmail.com>
2023-10-12 19:48:23 -0700

    header updates

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,6 +1,8 @@
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import Logo from '../../public/img/logo.svg'
+
 const Header = () => {
+    const location = useLocation()
   return (
     <header className='header'>
        <div className="contenedor barra">
@@ -8,10 +10,22 @@ const Header = () => {
                 <img className='logo' src={Logo} alt="GuitarLA" />
             </Link>
             <nav className='navegacion'>
-                <Link to='/'>Home</Link>
-                <Link to='/us'>Us</Link>
-                <Link to='/store'>Store</Link>
-                <Link to='/blog'>Blog</Link>
+                <Link 
+                to='/'
+                className={location.pathname === '/'?'active':''}
+                >Home</Link>
+                <Link 
+                to='/us'
+                className={location.pathname === '/us'?'active':''}
+                >Us</Link>
+                <Link 
+                to='/store'
+                className={location.pathname === '/store'?'active':''}
+                >Store</Link>
+                <Link 
+                to='/blog'
+                className={location.pathname === '/blog'?'active':''}
+                >Blog</Link>
             </nav>
        </div>
     </header>
